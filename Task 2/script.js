659f75e4e1ca838212f58860372660735d528ba6
albertoviciute <99351981+albertoviciute@users.noreply.github.com>
2022-05-02 14:49:17 +0300

    Add files via upload

---
+++ Task 2/script.js
@@ -0,0 +1,10 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+let count = 1;
+document.getElementById("btn__element").onclick = ()=>{
+    document.getElementById("btn__state").textContent = count++;
+}
+
