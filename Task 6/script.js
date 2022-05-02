7fe06bd0860bca1808f8aee66e2587823249bbeb
albertoviciute <99351981+albertoviciute@users.noreply.github.com>
2022-05-02 14:52:21 +0300

    Add files via upload

---
+++ Task 6/script.js
@@ -0,0 +1,37 @@
+/* ------------------------------ TASK 6 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funckijas, kurios atlikas nurodytas užduotis
+1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
+2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: '1', name: 'John Smith', age: 20 },
+  { id: '2', name: 'Ann Smith', age: 24 },
+  { id: '3', name: 'Tom Jones', age: 31 },
+  { id: '4', name: 'Rose Peterson', age: 17 },
+  { id: '5', name: 'Alex John', age: 25 },
+  { id: '6', name: 'Ronald Jones', age: 63 },
+  { id: '7', name: 'Elton Smith', age: 16 },
+  { id: '8', name: 'Simon Peterson', age: 30 },
+  { id: '9', name: 'Daniel Cane', age: 51 },
+];
+
+
+
+function getUserAverageAge(usersA){
+  const average = usersA.reduce((a,b) => a + b.age, 0) /usersA.length;
+  return console.log(average);
+}
+
+getUserAverageAge(users);
+
+function getUsersName(usersB){
+  const usersNames = usersB.map(usersB => usersB.name);
+  return console.log(usersNames);
+}
+
+getUsersName(users);
